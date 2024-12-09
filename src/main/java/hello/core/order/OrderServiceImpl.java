40ee8c28d94aaa6fe934ebd820741f18c92020e6
baesm971013 <bobbyb537@naver.com>
2024-12-09 21:01:48 +0900

    feat:  add Qualifier

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -6,24 +6,20 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // final 을 넣어야함
     private final DiscountPolicy discountPolicy; // 추상화인 interface에만 의존!!
 
-    //생성자 1개일떄는 autowired 생략 가능함.
-
-//    @Autowired
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-//        System.out.println("first");
-//        this.discountPolicy = discountPolicy;
-//        this.memberRepository = memberRepository;
-//    }
-
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy){
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
